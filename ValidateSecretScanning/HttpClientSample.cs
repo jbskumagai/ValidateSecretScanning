6cde984e4fbf506aae0d57ccc124d19b3dab182b
JBS\yutkumagai <yutkumagai@jbs.com>
2022-07-11 11:20:52 +0900

    [add]httpclient追加。

---
+++ ValidateSecretScanning/HttpClientSample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidateSecretScanning
+{
+    public class HttpClientSample
+    {
+        public async Task<HttpStatusCode> Get()
+        {
+            using (var client = new HttpClient())
+            {
+                var token = "RfeyoeHI+gK5JoqQ/6+kZn46DIA//wUxNDmJNeEoCbE=";
+                client.DefaultRequestHeaders.Add("x-token", token);
+                var result = await client.GetAsync("https://httpstat.us/200");
+                return result.StatusCode;
+            }
+        }
+    }
+}
--- ValidateSecretScanning/Program.cs
+++ ValidateSecretScanning/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ValidateSecretScanning;
+
 Console.WriteLine("Hello, World!");
+var client = new HttpClientSample();
+Console.WriteLine(await client.Get());
